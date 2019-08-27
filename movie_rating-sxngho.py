8b4fe0e188874facbd02b5e4ca6f82bd804cd115
Park Sangho <sxngho@naver.com>
2019-08-27 10:27:17 +0900

    [#2] S1P2242009-31 tokenization 구현 > 트레이닝, 테스트의 한글 문장 데이터를 KoNLPy를 사용한 tokeniza() 함수를 이용하여 tokenization 구현 > Input : 텍스트 데이터 > Output : ['token1/tagging','token2/tagging',...] 토큰에 해당되는 품사를 태깅

--- movie_rating-sxngho.py
+++ movie_rating-sxngho.py
@@ -12,25 +12,23 @@ read_data(): 데이터를 읽어서 저장하는 함수
 """
 
 def read_data(filename):
-    file = open(filename, 'r', encoding='utf-8')
-    datas = file.readlines()
-    res = []
-    for data in datas:
-        res.append(data)
-    return res
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = [line.split('\t') for line in f.read().splitlines()]
+        data = data[1:]   # header 제외
+    return data
 
 """
 Req 1-1-2. 토큰화 함수
 tokenize(): 텍스트 데이터를 받아 KoNLPy의 okt 형태소 분석기로 토크나이징
 """
-#
-# def tokenize(doc):
-#     return
-#
-# """
-# 데이터 전 처리
-# """
-#
+okt = Okt()
+def tokenize(doc):
+    return ['/'.join(t) for t in okt.pos(doc, norm=True, stem=True)]
+
+"""
+데이터 전 처리
+"""
+
 # train, test 데이터 읽기
 train_data = read_data('ratings_train.txt')
 test_data = read_data('ratings_test.txt')
@@ -39,8 +37,9 @@ test_data = read_data('ratings_test.txt')
 #
 # # Req 1-1-2. 문장 데이터 토큰화
 # # train_docs, test_docs : 토큰화된 트레이닝, 테스트  문장에 label 정보를 추가한 list
-# train_docs = None
-# test_docs = None
+# train_docs = [(tokenize(row[1]), row[2]) for row in train_data]
+test_docs = [(tokenize(row[1]), row[2]) for row in test_data]
+
 #
 #
 # # Req 1-1-3. word_indices 초기화
