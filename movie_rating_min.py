4b00e965427f6422e4bec70cb836a5f4f87c24f6
min <smp1991@naver.com>
2019-08-27 17:52:28 +0900

    [#2] S1P2242009-35 One-hot 임베딩 적용 word_indices 행렬을 사용하여 토큰화 된 트레이닝, 테스트 데이터를 One-hot 임베딩 적용 > X, X_test에 One-hot 임베딩 값 채우기 > Y, Y_test에 label값 채우기

--- movie_rating_min.py
+++ movie_rating_min.py
@@ -60,3 +60,27 @@ X_test = lil_matrix(len(test_docs), len(word_indices))
 # Y_test: test data label
 Y = np.zeros(len(train_docs))
 Y_test = np.zeros(len(test_docs))
+
+# Req 1-1-5. one-hot 임베딩
+# X,Y 벡터값 채우기
+
+def one_hot_encoding(vocas, word_indice):
+    one_hot_vector = [0] * (len(word_indices))
+    for (idx, voca) in enumerate(vocas[0]):
+        text = voca.split('/')[0]
+        index = word_indices.get(text)
+        if index is not None:
+            one_hot_vector[index] = 1
+    return one_hot_vector
+
+for (idx1, vocas) in enumerate(train_docs):
+    X[idx1] = one_hot_encoding(vocas, word_indices)
+
+for (idx1, vocas) in enumerate(test_docs):
+    X_test[idx1] = one_hot_encoding(vocas, word_indices)
+​
+for (idx1, vocas) in enumerate(train_docs):
+     Y[idx1] = vocas[1]
+
+for (idx1, vocas) in enumerate(test_docs):
+     Y_test[idx1] = vocas[1]
