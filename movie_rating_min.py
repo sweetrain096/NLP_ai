ace1625609f9d5be5ed0069eb1b02479c5d98dfb
min <smp1991@naver.com>
2019-08-27 17:58:28 +0900

    [#4] S1P2242009-41 예측된 결과값 출력 특정 문장 데이터와 Navie bayes classifier, Logistic regression 모델의 정확도 계산 > 예측된 분류값과 실제 label사이의 오차의 평균을 계산(관련 함수를 찾아 사용)

--- movie_rating_min.py
+++ movie_rating_min.py
@@ -98,3 +98,11 @@ clf.fit(X, Y)
 # Req 1-2-2. Logistic regression model 학습
 clf2 = LogisticRegression()
 clf2.fit(X, Y)
+
+"""
+테스트 파트
+"""
+​
+# Req 1-3-1. 문장 데이터에 따른 예측된 분류값 출력
+print("Naive bayesian classifier example result: {}, {}".format(test_data[3][1], clf.predict(X_test[3])))
+print("Logistic regression exampleresult: {}, {}".format(test_data[3][1], clf2.predict(X_test[3])))
