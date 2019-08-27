f5df25256f85e6a379a4a5d9969dc69f924e3d06
min <smp1991@naver.com>
2019-08-27 17:22:47 +0900

    [#2] S1P2242009-27 word_indices 행렬을 생성 단어 사전 정보가 저장된 word_indices 행렬을 생성 > word_indices : token 사전 딕셔너리   (token1 : 1, token2 : 2, ...) 다시 수정

--- movie_rating_min.py
+++ movie_rating_min.py
@@ -13,8 +13,7 @@ read_data(): 데이터를 읽어서 저장하는 함수
 
 def read_data(filename):
     with open(filename, 'r', encoding='utf-8') as f:
-        data = [line.split('\t') for line in f.read().splitlines()]
-        data = data[1:] # header 제외
+        data = [line.split('\t') for line in f.read().splitlines()][1:]
     return data
 
 """
@@ -35,18 +34,36 @@ def tokenize(doc):
 train_data = read_data('ratings_train.txt')
 test_data = read_data('ratings_test.txt')
 
-
 # Req 1-1-2. 문장 데이터 토큰화
 # train_docs, test_docs : 토큰화된 트레이닝, 테스트  문장에 label 정보를 추가한 list
-train_docs = [tokenize(row[1]) for row in train_data]
+train_docs = [(tokenize(row[1]), row[2]) for row in train_data]
 test_docs = [(tokenize(row[1]), row[2]) for row in test_data]
 
 # Req 1-1-3. word_indices 초기화
 word_indices = {}
 
 # Req 1-1-3. word_indices 채우기
-for voca in train_docs[0]:
-    if voca not in word_indices.keys():
-        word_indices[voca]=len(word_indices)
+for vocas in train_docs:
+    for voca in vocas[0]:
+        text = voca.split('/')[0]
+        if text not in word_indices:
+            word_indices[text] = len(word_indices)
 
 print(word_indices)
+    # if voca not in word_indices.keys():
+    #     word_indices[voca]=len(word_indices)
+
+# print(word_indices)
+# train_tokens = [t for d in train_docs for t in d]
+
+# Req 1-1-4. sparse matrix 초기화
+# X: train feature data
+# X_test: test feature data
+X = None
+X_test = None
+
+# 평점 label 데이터가 저장될 Y 행렬 초기화
+# Y: train data label
+# Y_test: test data label
+Y = None
+Y_test = None
