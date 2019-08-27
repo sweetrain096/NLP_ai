125f51331b9599604995f8b1e945d5205a510905
min <smp1991@naver.com>
2019-08-27 17:59:19 +0900

    [#4] S1P2242009-77 정확도 계산 전 처리된 테스트 데이터를 통하여 Naive bayes classifier, Logistic regresson 모델의 정확도 계산 예측된 분류 값과 실제 label 사이의 오차의 평균을 계산 (관련 함수를 찾아 사용)

--- movie_rating_min.py
+++ movie_rating_min.py
@@ -106,3 +106,7 @@ clf2.fit(X, Y)
 # Req 1-3-1. 문장 데이터에 따른 예측된 분류값 출력
 print("Naive bayesian classifier example result: {}, {}".format(test_data[3][1], clf.predict(X_test[3])))
 print("Logistic regression exampleresult: {}, {}".format(test_data[3][1], clf2.predict(X_test[3])))
+
+# Req 1-3-2. 정확도 출력
+print("Naive bayesian classifier accuracy: {}".format(clf.score(X_test, Y_test)))
+print("Logistic regression accuracy: {}".format(clf2.score(X_test, Y_test)))
