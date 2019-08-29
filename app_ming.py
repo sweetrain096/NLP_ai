012fe480ace6fe8c3b1ed1476b653aabd63e5b49
이민지 <aksenskf0k@naver.com>
2019-08-29 14:33:16 +0900

    [#7] S1P2242009-24 app.db를 연동하여 웹에서 주고받는 데이터를 DB로 저장

--- app_ming.py
+++ app_ming.py
@@ -34,40 +34,58 @@ with open('model.clf', 'rb') as f:
 # Req 2-2-2. 토큰화 및 one-hot 임베딩하는 전 처리
 # input : 영화평 문장
 # output : 전 처리된 sparse matrix
-text = '핵노잼'
+text = '너무 재미있음'
 def preprocess(text):
     okt = Okt()
     token = okt.pos(text)
     X = lil_matrix((1, len(word_indices)+1))
     # lil_matrix는 선언 할 때 괄호 두개, 1행짜리면 뒤에 , 1 붙여주기
-    print(X)
+    # print(X)
     for voca in token:
         indices = word_indices.get('/'.join(voca))
+        # print(indices)
         if indices:
             X[0, indices] = 1
+    # print(X)
     return X
-print(preprocess(text))
-
+preprocess(text)
 # Req 2-2-3. 긍정 혹은 부정으로 분류
 # input : 전 처리된 sparse matrix
 # output : 부정적 리뷰 or 긍적적 리뷰(다양한 방식으로 분류 값 사용 가능)
-def classify(X):
-    if clf.predict(X):
+def classify(text):
+    data = preprocess(text)
+    # print(data)
+    result = clf.predict(data)[0]
+
+    if result:
         return '긍정'
     return '부정'
 
-X = preprocess(text)
 # print('X', X)
-print(classify(X))
-'''
+
 # Req 2-2-4. app.db 를 연동하여 웹에서 주고받는 데이터를 DB로 저장
-    
+def insert(text):
+    conn = sqlite3.connect('app.db')
+    c = conn.cursor()
+    print(text)
+    c.execute('''INSERT INTO search_history(query) VALUES(?)''', (text,))
+    conn.commit()
+    conn.close()
 
 # 챗봇이 멘션을 받았을 경우
 @slack_events_adaptor.on("app_mention")
 def app_mentioned(event_data):
     channel = event_data["event"]["channel"]
     text = event_data["event"]["text"]
+    text = text.split('>')[1]
+    print(text)
+
+    keywords = classify(text)
+    # insert(text)
+    slack_web_client.chat_postMessage(
+        channel=channel,
+        text=keywords
+    )
 
 
 @app.route("/", methods=["GET"])
@@ -76,4 +94,3 @@ def index():
 
 if __name__ == '__main__':
     app.run()
-'''
