94ef1163783c7f2d014f0a4e0c6c19ec11f58458
sweetrain096 <gpfhddl09@gmail.com>
2019-08-29 11:52:13 +0900

    [#7] S1P2242009-21 pickle로 저장된 model.clf 파일 불러오기

--- app_rain.py
+++ app_rain.py
@@ -1,7 +1,7 @@
 import pickle
+import os
 from threading import Thread
 import sqlite3
-from model import Model
 
 import numpy as np
 from konlpy.tag import Okt
@@ -12,9 +12,8 @@ from scipy.sparse import lil_matrix
 
 
 # slack 연동 정보 입력 부분
-SLACK_TOKEN = "xoxb-619154581858-619130324707-N5IswN8gy8ZO9mcIxVYPhrkP"
-SLACK_SIGNING_SECRET = "2e319790605d216c629f5c107aa16c2a"
-
+SLACK_TOKEN = os.getenv("SLACK_TOKEN")
+SLACK_SIGNING_SECRET = os.getenv("SLACK_SIGNING_SECRET")
 app = Flask(__name__)
 
 slack_events_adaptor = SlackEventAdapter(SLACK_SIGNING_SECRET, "/listening", app)
@@ -23,14 +22,12 @@ slack_web_client = WebClient(token=SLACK_TOKEN)
 
 # Req 2-2-1. pickle로 저장된 model.clf 파일 불러오기
 with open('model.clf', 'rb') as f:
-    model = pickle.load(f)
-# word_indices = model.get_word_indices()
-# clf = model.get_naive_model()
-# clf2 = model.get_logistic_model()
+    pickle_obj = pickle.load(f)
 
-pickle_obj = None
-word_indices = None
-clf = None
+word_indices = pickle_obj.get_word_indices()
+clf = pickle_obj.get_naive_model()
+clf2 = pickle_obj.get_logistic_model()
+
 
 # Req 2-2-2. 토큰화 및 one-hot 임베딩하는 전 처리
 def preprocess():
@@ -43,7 +40,7 @@ def classify():
     return None
     
 # Req 2-2-4. app.db 를 연동하여 웹에서 주고받는 데이터를 DB로 저장
-    
+
 
 # 챗봇이 멘션을 받았을 경우
 @slack_events_adaptor.on("app_mention")
