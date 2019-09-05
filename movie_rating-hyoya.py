fec90203a9cdfb473a3665628e31e50a8093e6a9
hyoya <ghtybndss@gmail.com>
2019-09-05 17:23:31 +0900

    [#11] S1P2242009-52 log_likelihoods_naivebayes() 함수 구현 Likelihood 확률의 로그 값을 출력하는 log_likelihoods_naivebayes() 함수 구현  > Input :     feature_vector : 단어 사전 크기를 갖는 벡터     one-hot 임베딩을 사용했기에 0 or 1 성분 값을 가짐     Class : label 값인0 or 1  > Output :     Class에 해당되는 likelihood 확률의 로그값     = log ( P ( feature_vector | class ) )

--- movie_rating-hyoya.py
+++ movie_rating-hyoya.py
@@ -8,7 +8,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 
-'''
+
 """
 Req 1-1-1. 데이터 읽기
 read_data(): 데이터를 읽어서 저장하는 함수
@@ -89,27 +89,27 @@ for n in range(len(test_docs)):
             X_test[n, indices] = 1
     Y_test[n] = test_docs[n][1]
 
-##########################################
-class saveData :
-    def __init__(self):
-        self.X_test = None
-        self.Y_test = None
-        self.X = None
-        self.Y = None
-
-    def initdata(self, X, Y, X_test, Y_test):
-        self.X = X
-        self.Y = Y
-        self.X_test = X_test
-        self.Y_test = Y_test
-
-sd = saveData()
-sd.initdata(X,Y,X_test,Y_test)
-with open('tempData_hyoya.clf', 'wb') as f:
-   pickle.dump(sd, f)
-
-print("Alright, Save Complete")
-##############################################
+# ##########################################
+# class saveData :
+#     def __init__(self):
+#         self.X_test = None
+#         self.Y_test = None
+#         self.X = None
+#         self.Y = None
+#
+#     def initdata(self, X, Y, X_test, Y_test):
+#         self.X = X
+#         self.Y = Y
+#         self.X_test = X_test
+#         self.Y_test = Y_test
+#
+# sd = saveData()
+# sd.initdata(X,Y,X_test,Y_test)
+# with open('tempData_hyoya.clf', 'wb') as f:
+#    pickle.dump(sd, f)
+#
+# print("Alright, Save Complete")
+# ##############################################
 
 """
 트레이닝 파트
@@ -160,7 +160,7 @@ model.set_word_indices(word_indices)
 
 with open('model_add_knn.clf', 'wb') as f:
    pickle.dump(model, f)
-'''
+
 
 
 # Naive bayes classifier algorithm part
@@ -169,159 +169,159 @@ with open('model_add_knn.clf', 'wb') as f:
 #########################################################################
 #########################################################################
 
-# """
-# Naive_Bayes_Classifier 알고리즘 클래스입니다.
-# """
-#
-# class Naive_Bayes_Classifier(object):
-#
-#     """
-#     Req 3-1-1.
-#     log_likelihoods_naivebayes():
-#     feature 데이터를 받아 label(class)값에 해당되는 likelihood 값들을
-#     naive한 방식으로 구하고 그 값의 log값을 리턴
-#     """
-#
-#     def log_likelihoods_naivebayes(self, feature_vector, Class):
-#         log_likelihood = 0.0
-#
-#         if Class == 0:
-#             for feature_index in range(len(feature_vector)):
-#                 if feature_vector[feature_index] == 1: #feature present
-#                     log_likelihood += None
-#                 elif feature_vector[feature_index] == 0: #feature absent
-#                     log_likelihood += None
-#         elif Class == 1:
-#             for feature_index in range(len(feature_vector)):
-#                 if feature_vector[feature_index] == 1:
-#                     log_likelihood += None
-#                 elif feature_vector[feature_index] == 0:
-#                     log_likelihood += None
-#
-#         return None
-#
-#     """
-#     Req 3-1-2.
-#     class_posteriors():
-#     feature 데이터를 받아 label(class)값에 해당되는 posterior 값들을
-#     구하고 그 값의 log값을 리턴
-#     """
-#
-#     def class_posteriors(self, feature_vector):
-#         log_likelihood_0 = self.log_likelihoods_naivebayes(feature_vector, Class = 0)
-#         log_likelihood_1 = self.log_likelihoods_naivebayes(feature_vector, Class = 1)
-#
-#         log_posterior_0 = None
-#         log_posterior_1 = None
-#
-#         return None
-#
-#     """
-#     Req 3-1-3.
-#     classify():
-#     feature 데이터에 해당되는 posterir값들(class 개수)을 불러와 비교하여
-#     더 높은 확률을 갖는 class를 리턴
-#     """
-#
-#     def classify(self, feature_vector):
-#         return None
-#
-#     """
-#     Req 3-1-4.
-#     train():
-#     트레이닝 데이터를 받아 학습하는 함수
-#     학습 후, 각 class에 해당하는 prior값과 likelihood값을 업데이트
-#
-#     알고리즘 구성
-#     1) 가중치 값인 beta_x_i, beta_c_i 초기화
-#     2) Y label 데이터 reshape
-#     3) 가중치 업데이트 과정 (iters번 반복)
-#     3-1) prediction 함수를 사용하여 error 계산
-#     3-2) gadient_beta 함수를 사용하여 가중치 값 업데이트
-#     4) 최적화 된 가중치 값들 리턴
-#        self.beta_x, self.beta_c
-#     """
-#
-#     def train(self, X, Y):
-#         # label 0에 해당되는 데이터의 개수 값(num_0) 초기화
-#         num_0 = 0
-#         # label 1에 해당되는 데이터의 개수 값(num_1) 초기화
-#         num_1 = 0
-#
-#         # Req 3-1-7. smoothing 조절
-#         # likelihood 확률이 0값을 갖는것을 피하기 위하여 smoothing 값 적용
-#         smoothing = None
-#
-#         # label 0에 해당되는 각 feature 성분의 개수값(num_token_0) 초기화
-#         num_token_0 = np.zeros((1,X.shape[1]))
-#         # label 1에 해당되는 각 feature 성분의 개수값(num_token_1) 초기화
-#         num_token_1 = np.zeros((1,X.shape[1]))
-#
-#
-#         # 데이터의 num_0,num_1,num_token_0,num_token_1 값 계산
-#         for i in range(X.shape[0]):
-#             if (Y[i] == 0):
-#                 num_0 += 1
-#                 num_token_0 += None
-#
-#             if (Y[i] == 1):
-#                 num_1 += 1
-#                 num_token_1 += None
-#
-#         # smoothing을 사용하여 각 클래스에 해당되는 likelihood값 계산
-#         self.likelihoods_0 = None
-#         self.likelihoods_1 = None
-#
-#         # 각 class의 prior를 계산
-#         prior_probability_0 = None
-#         prior_probability_1 = None
-#
-#         # pior의 log값 계
-#         self.log_prior_0 = None
-#         self.log_prior_1 = None
-#
-#         return None
-#
-#     """
-#     Req 3-1-5.
-#     predict():
-#     테스트 데이터에 대해서 예측 label값을 출력해주는 함수
-#     """
-#
-#     def predict(self, X_test):
-#         predictions = []
-#         X_test=X_test.toarray()
-#         if (len(X_test)==1):
-#             predictions.append(None)
-#         else:
-#             for case in X_test:
-#                 predictions.append(None)
-#
-#         return predictions
-#
-#     """
-#     Req 3-1-6.
-#     score():
-#     테스트를 데이터를 받아 예측된 데이터(predict 함수)와
-#     테스트 데이터의 label값을 비교하여 정확도를 계산
-#     """
-#
-#     def score(self, X_test, Y_test):
-#
-#         return None
-#
-# # Req 3-2-1. model에 Naive_Bayes_Classifier 클래스를 사용하여 학습합니다.
-# model = None
-#
-# # Req 3-2-2. 정확도 측정
-# print("Naive_Bayes_Classifier accuracy: {}".format(None))
-#
-# # Logistic regression algorithm part
-# # 아래의 코드는 심화 과정이기에 사용하지 않는다면 주석 처리하고 실행합니다.
-#
+"""
+Naive_Bayes_Classifier 알고리즘 클래스입니다.
+"""
+
+class Naive_Bayes_Classifier(object):
+
+    """
+    Req 3-1-1.
+    log_likelihoods_naivebayes():
+    feature 데이터를 받아 label(class)값에 해당되는 likelihood 값들을
+    naive한 방식으로 구하고 그 값의 log값을 리턴
+    """
+
+    def log_likelihoods_naivebayes(self, feature_vector, Class):
+        log_likelihood = 0.0
+        if Class == 0:
+            for feature_index in range(len(feature_vector)):
+                if feature_vector[feature_index] == 1:  # feature present
+                    log_likelihood += self.log_prior_0[0, feature_index]
+                elif feature_vector[feature_index] == 0:  # feature absent
+                    log_likelihood += 0
+        elif Class == 1:
+            for feature_index in range(len(feature_vector)):
+                if feature_vector[feature_index] == 1:
+                    log_likelihood += self.log_prior_1[0, feature_index]
+                elif feature_vector[feature_index] == 0:
+                    log_likelihood += 0
+        return log_likelihood
+
+    """
+    Req 3-1-2.
+    class_posteriors():
+    feature 데이터를 받아 label(class)값에 해당되는 posterior 값들을
+    구하고 그 값의 log값을 리턴
+    """
+
+    def class_posteriors(self, feature_vector):
+        log_likelihood_0 = self.log_likelihoods_naivebayes(feature_vector, Class = 0)
+        log_likelihood_1 = self.log_likelihoods_naivebayes(feature_vector, Class = 1)
+
+        log_posterior_0 = None
+        log_posterior_1 = None
+
+        return None
+
+    """
+    Req 3-1-3.
+    classify():
+    feature 데이터에 해당되는 posterir값들(class 개수)을 불러와 비교하여
+    더 높은 확률을 갖는 class를 리턴
+    """
+
+    def classify(self, feature_vector):
+        return None
+
+    """
+    Req 3-1-4.
+    train():
+    트레이닝 데이터를 받아 학습하는 함수
+    학습 후, 각 class에 해당하는 prior값과 likelihood값을 업데이트
+
+    알고리즘 구성
+    1) 가중치 값인 beta_x_i, beta_c_i 초기화
+    2) Y label 데이터 reshape
+    3) 가중치 업데이트 과정 (iters번 반복)
+    3-1) prediction 함수를 사용하여 error 계산
+    3-2) gadient_beta 함수를 사용하여 가중치 값 업데이트
+    4) 최적화 된 가중치 값들 리턴
+       self.beta_x, self.beta_c
+    """
+
+    def train(self, X, Y):
+        # label 0에 해당되는 데이터의 개수 값(num_0) 초기화
+        num_0 = 0
+        # label 1에 해당되는 데이터의 개수 값(num_1) 초기화
+        num_1 = 0
+
+        # Req 3-1-7. smoothing 조절
+        # likelihood 확률이 0값을 갖는것을 피하기 위하여 smoothing 값 적용
+        smoothing = None
+
+        # label 0에 해당되는 각 feature 성분의 개수값(num_token_0) 초기화
+        num_token_0 = np.zeros((1,X.shape[1]))
+        # label 1에 해당되는 각 feature 성분의 개수값(num_token_1) 초기화
+        num_token_1 = np.zeros((1,X.shape[1]))
+
+
+        # 데이터의 num_0,num_1,num_token_0,num_token_1 값 계산
+        for i in range(X.shape[0]):
+            if (Y[i] == 0):
+                num_0 += 1
+                num_token_0 += None
+
+            if (Y[i] == 1):
+                num_1 += 1
+                num_token_1 += None
+
+        # smoothing을 사용하여 각 클래스에 해당되는 likelihood값 계산
+        self.likelihoods_0 = None
+        self.likelihoods_1 = None
+
+        # 각 class의 prior를 계산
+        prior_probability_0 = None
+        prior_probability_1 = None
+
+        # pior의 log값 계
+        self.log_prior_0 = None
+        self.log_prior_1 = None
+
+        return None
+
+    """
+    Req 3-1-5.
+    predict():
+    테스트 데이터에 대해서 예측 label값을 출력해주는 함수
+    """
+
+    def predict(self, X_test):
+        predictions = []
+        X_test=X_test.toarray()
+        if (len(X_test)==1):
+            predictions.append(None)
+        else:
+            for case in X_test:
+                predictions.append(None)
+
+        return predictions
+
+    """
+    Req 3-1-6.
+    score():
+    테스트를 데이터를 받아 예측된 데이터(predict 함수)와
+    테스트 데이터의 label값을 비교하여 정확도를 계산
+    """
+
+    def score(self, X_test, Y_test):
+
+        return None
+
+# Req 3-2-1. model에 Naive_Bayes_Classifier 클래스를 사용하여 학습합니다.
+model = None
+
+# Req 3-2-2. 정확도 측정
+print("Naive_Bayes_Classifier accuracy: {}".format(None))
+
+# Logistic regression algorithm part
+# 아래의 코드는 심화 과정이기에 사용하지 않는다면 주석 처리하고 실행합니다.
+
 
 #########################################################################
 #########################################################################
+
+'''
 """
 Logistic_Regression_Classifier 알고리즘 클래스입니다.
 """
@@ -368,10 +368,11 @@ class Logistic_Regression_Classifier(object):
     """
 
     def prediction(self, beta_x, beta_c, X):
-        # 예측 확률 P(class=1)을 계산하는 식을 만든다.
-        equation = X@beta_x + beta_c;
-    
-        return equation.reshape(-1, 1)
+        # z = X * beta_x.T, beta_c
+        # return self.sigmoid(z)
+        tmp = np.dot(X, np.transpose(beta_x,)) + beta_c
+        return self.sigmoid(tmp)
+
     """
     Req 3-3-3.
     gradient_beta():
@@ -478,3 +479,4 @@ print("Logistic_Regression_Classifier accuracy: {}".format(model2.score(X_test, 
 
 #########################################################################
 #########################################################################
+'''
