3e1a23787933c8e72d45a787f58c07444af84e7a
Park Sangho <sxngho@naver.com>
2019-09-06 11:35:41 +0900

    [#12] S1P2242009-68 train() 함수 구현  > Input : 학습용 X 데이터, 학습용 label 데이터

--- movie_rating-sxngho.py
+++ movie_rating-sxngho.py
@@ -372,29 +372,25 @@ class Logistic_Regression_Classifier(object):
     def train(self, X, Y):
         # Req 3-3-8. learning rate 조절
         # 학습률(learning rate)를 설정한다.(권장: 1e-3 ~ 1e-6)
-        lr = 1e-2
+        lr = 1e-3
         # 반복 횟수(iteration)를 설정한다.(자연수)
         iters = 200
-
         # beta_x, beta_c값을 업데이트 하기 위하여 beta_x_i, beta_c_i값을 초기화
-        beta_x_i = None
-        beta_c_i = None
-
-        #행렬 계산을 위하여 Y데이터의 사이즈를 (len(Y),1)로 저장합니다.
-        Y=None
-
+        beta_x_i = np.zeros((1, X.shape[1]))
+        beta_c_i = 0
+        # 행렬 계산을 위하여 Y데이터의 사이즈를 (len(Y),1)로 저장합니다.
+        Y = Y.reshape((len(Y), 1))
         for i in range(iters):
-            #실제 값 Y와 예측 값의 차이를 계산하여 error를 정의합니다.
-            error = None
-            #gredient_beta함수를 통하여 델타값들을 업데이트 합니다.
-            beta_x_delta, beta_c_delta = self.gradient_beta(None)
-            beta_x_i -= beta_x_delta.T
+            # 실제 값 Y와 예측 값의 차이를 계산하여 error를 정의합니다.
+            pred = self.prediction(beta_x_i, beta_c_i, X)
+            error = pred - Y
+            # gredient_beta함수를 통하여 델타값들을 업데이트 합니다.
+            beta_x_delta, beta_c_delta = self.gradient_beta(X, error, lr)
+            beta_x_i -= beta_x_delta
             beta_c_i -= beta_c_delta
-
         self.beta_x = beta_x_i
         self.beta_c = beta_c_i
-
-        return None
+        return self.beta_x, self.beta_c
 
     """
     Req 3-3-5.
